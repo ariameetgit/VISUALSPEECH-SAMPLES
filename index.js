b1c0f0536b361836fba54166bd1df77515dd0240
ariameetgit <138862940+ariameetgit@users.noreply.github.com>
2025-02-11 13:51:44 +0000

    Update index.js
    changing the paper title

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-      document.getElementById('main-content').innerHTML = 'Audio samples from "VisualSpeech: Improve Speech Synthesis with Visual Features"'
+      document.getElementById('main-content').innerHTML = 'VisualSpeech: Enhancing Prosody Modeling in TTS Using Video'
 }
